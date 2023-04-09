0ced51ec9b892903f620e0d6163e9542e6e8ec36
Krake24 <krake24@mail.de>
2023-04-09 18:21:28 +0200

    created myria token watcher

---
+++ main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import os
+import disnake
+from disnake.ext import commands, tasks
+import requests
+
+bot = commands.InteractionBot()
+
+@bot.event
+async def on_ready():
+    if not loop.is_running():
+        loop.start()
+
+@tasks.loop(seconds=5)
+async def loop():
+    result = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=myria&vs_currencies=USD&include_24hr_change=true").json()
+    usd_value=result['myria']['usd']
+    usd_change=result['myria']['usd_24h_change']
+    await bot.change_presence(activity=disnake.Game(name=f'{usd_change:.0f}%'))
+    for guild in bot.guilds:
+        await guild.me.edit(nick=f'MYRIA: ${usd_value:.5f}')
+
+bot.run(os.getenv("BOT_TOKEN"))
