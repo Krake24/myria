1ac585f0a6dd15202b4a452a80d6794df92c1ebb
Krake24 <krake24@mail.de>
2023-04-23 21:50:10 +0000

    prevents errors from breaking the bto

--- main.py
+++ main.py
@@ -13,11 +13,14 @@ async def on_ready():
 
 @tasks.loop(seconds=5)
 async def loop():
-    result = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=myria&vs_currencies=USD&include_24hr_change=true").json()
-    usd_value=result['myria']['usd']
-    usd_change=result['myria']['usd_24h_change']
-    await bot.change_presence(activity=disnake.Game(name=f'24h: {usd_change:.0f}%'))
-    for guild in bot.guilds:
-        await guild.me.edit(nick=f'MYRIA: ${usd_value:.5f}')
+    try:
+        result = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=myria&vs_currencies=USD&include_24hr_change=true").json()
+        usd_value=result['myria']['usd']
+        usd_change=result['myria']['usd_24h_change']
+        await bot.change_presence(activity=disnake.Game(name=f'24h: {usd_change:.0f}%'))
+        for guild in bot.guilds:
+            await guild.me.edit(nick=f'MYRIA: ${usd_value:.5f}')
+    except:
+        print("error trying to get current value")
 
 bot.run(os.getenv("BOT_TOKEN"))
